0cd31b4c9517fb650723725010b2ee8aa98a06da
Nicholas Grasevski <nicholas.grasevski@ocius.com.au>
2025-09-23 09:02:49 +1000

    feat(design): revision 2
    Refactored the design to work with either attiny9 or attiny25, using a
    basic hardware abstraction layer. So that the PCB can have a tagconnect
    footprint rather than excessive through holes and plugging and
    unplugging of attiny85 into a socket.

--- gc2n64.s
+++ gc2n64.s
@@ -1,25 +1,96 @@
-; Adapter to convert gamecube controller to N64
+; Adapter to convert gamecube controller to N64.
+.equ STUB_GCC = 1
+;.device attiny9
+;.equ TINY9 = 1
 .device attiny25
-.equ PCMSK = 0x15
-.equ PINB = 0x16
-.equ DDRB = 0x17
-.equ PORTB = 0x18
-.equ WDTCR = 0x21
-.equ CLKPR = 0x26
-.equ OCR0A = 0x29
-.equ OSCCAL = 0x31
-.equ TCNT0 = 0x32
-.equ TCCR0B = 0x33
-.equ GIMSK = 0x3b
-.equ TIMSK = 0x39
+
+; Hardware Abstraction Layer.
+.ifdef TINY9
+    ; Register mapping.
+    .equ PINB = 0x00
+    .equ DDRB = 0x01
+    .equ PORTB = 0x02
+    .equ PCMSK = 0x10
+    .equ PCIFR = 0x11
+    .equ PCICR = 0x12
+    .equ OCR0AL = 0x26
+    .equ TCNT0L = 0x28
+    .equ TIMSK0 = 0x2b
+    .equ TCCR0B = 0x2d
+    .equ CLKPSR = 0x36
+    .equ OSCCAL = 0x39
+    .equ SMCR = 0x3a
+    .equ CCP = 0x3c
+
+    ; Peripheral pin mapping.
+    .equ PCIE0 = 0
+    .equ SE = 0
+    .equ SIG = 0xd8
+    .equ OCIE0A = 1
+    .equ PCIF0 = 0
+
+    ; Make it align with attiny25 timing.
+    .macro set_bit
+        nop
+        sbi @0, @1
+    .endm
+
+    ; Make it align with attiny25 timing.
+    .macro clr_bit
+        nop
+        cbi @0, @1
+    .endm
+.else
+    ; Register mapping.
+    .equ PINB = 0x16
+    .equ DDRB = 0x17
+    .equ PORTB = 0x18
+    .equ PCMSK = 0x15
+    .equ PCIFR = 0x3a
+    .equ PCICR = 0x3b
+    .equ OCR0AL = 0x29
+    .equ TCNT0L = 0x32
+    .equ TIMSK0 = 0x39
+    .equ TCCR0B = 0x33
+    .equ CLKPSR = 0x26
+    .equ OSCCAL = 0x31
+    .equ SMCR = 0x35
+    .equ CCP = CLKPSR
+
+    ; Peripheral pin mapping.
+    .equ PCIE0 = 5
+    .equ SE = 5
+    .equ SIG = 1 << 7
+    .equ OCIE0A = 4
+    .equ PCIF0 = 5
+
+    ; Wrapper to ensure cycle counts match.
+    .macro set_bit
+        sbi @0, @1
+    .endm
+
+    ; Wrapper to ensure cycle counts match.
+    .macro clr_bit
+        cbi @0, @1
+    .endm
+.endif
+
+; N64 data pin.
 .equ n64 = 0
+
+; Gamecube controller data pin.
 .equ gcc = 1
-.equ n64l = 3
-.equ gccl = 2
+
+; LED status pin.
+.equ led = 2
+
+; N64 response buffer.
 .def n64_0 = r16
 .def n64_1 = r17
 .def n64_2 = r18
 .def n64_3 = r19
+
+; Gamecube controller temp variables.
 .def gcc_0 = r20
 .def gcc_1 = r21
 .def gcc_2 = r22
@@ -38,13 +109,13 @@
     ldi gcc_0, 8
 ; Bit bang one bit.
 send_bit_%:
-    sbi DDRB, @0
+    set_bit DDRB, @0
     sbrs @1, 7
     rjmp send_0_%
     .if @0 == gcc
         nop
     .endif
-    cbi DDRB, @0
+    clr_bit DDRB, @0
     rjmp send_end_%
 ; Bit bang a 0 bit.
 send_0_%:
@@ -64,50 +135,26 @@ send_end_%:
         nop
         nop
     .endif
-    cbi DDRB, @0
+    clr_bit DDRB, @0
     .if @0 == gcc
         nop
     .endif
     brne send_bit_%
 .endm
 
-; Bit bang N64 stop bit to the console.
-.macro n64_send_stop
-    nop
-    sbi DDRB, n64
-    rjmp n64_stop_wait
-.endm
-
-; Read a bit from N64 console.
-.macro n64_read
-    sbrs gcc_0, 0
-    out OSCCAL, gcc_1
-    lsl gcc_0
-    in gcc_1, OSCCAL
-    delay_1_us
-    sbis PINB, n64
-    inc gcc_0
-    sbic PINB, n64
-    inc gcc_1
-    nop
-    nop
-    sbis PINB, n64
-    dec gcc_1
-.endm
-
-; Read bits from gamecube controller.
-.macro gcc_read_bits
-    ldi n64_0, @1
+; Read several bits.
+.macro read_bits
+    ldi @2, @3
 ; Read one bit.
 read_bit_%:
-    lsl @0
+    lsl @1
     nop
     nop
     nop
     delay_1_us
-    sbis PINB, gcc
-    inc @0
-    dec n64_0
+    sbic PINB, @0
+    inc @1
+    dec @2
     nop
     nop
     nop
@@ -116,7 +163,7 @@ read_bit_%:
 
 ; Read a byte from gamecube controller.
 .macro gcc_read
-    gcc_read_bits @0, 7
+    read_bits gcc, @0, n64_0, 8
 .endm
 
 ; Reset vector.
@@ -129,11 +176,6 @@ read_bit_%:
 
 ; TIMER0_COMPA interrupt when it is time to poll gamecube controller.
 .org 0x000a
-    dec gcc_1
-    brne poll_gcc
-    reti
-; Waited long enough to poll the controller.
-poll_gcc:
     ldi gcc_1, 0x40
     ldi gcc_2, 0x00
     ldi gcc_3, 0x03
@@ -141,11 +183,11 @@ poll_gcc:
     send_byte gcc, gcc_2
     send_byte gcc, gcc_3
     clr gcc_0
-    sbi DDRB, gcc
+    set_bit DDRB, gcc
     nop
     nop
     nop
-    cbi DDRB, gcc
+    clr_bit DDRB, gcc
 ; Detect the start of the gamecube controller response.
 wait_for_low:
     sbis PINB, gcc
@@ -155,20 +197,31 @@ wait_for_low:
 ; Read the gamecube response.
 read:
     clr gcc_0
-    nop
+    clr n64_1
     nop
     sbis PINB, gcc
-    sbr gcc_0, 0
+    inc gcc_0
+    nop
     nop
     delay_1_us
-    gcc_read_bits gcc_0, 7
+    read_bits gcc, gcc_0, n64_0, 7
     gcc_read gcc_1
     gcc_read n64_2
     gcc_read n64_3
     gcc_read gcc_2
     gcc_read gcc_3
     clr n64_0
-    clr n64_1
+
+    ; Put in all zero values for gamecube controller.
+    .ifdef STUB_GCC
+        clr gcc_0
+        clr gcc_1
+        clr gcc_2
+        clr gcc_3
+        ldi n64_2, 128
+        ldi n64_3, 128
+    .endif
+
     sbrc gcc_0, 0
     sbr n64_0, 7
     sbrc gcc_0, 1
@@ -228,24 +281,24 @@ not_c_up:
 
 ; Configures registers.
 reset:
-    ldi gcc_0, 0x80
-    out CLKPR, gcc_0
-    ldi gcc_0, 0x01
-    out CLKPR, gcc_0
-    sbi PCMSK, n64
-    ldi gcc_0, 0x20
-    out GIMSK, gcc_0
+    ldi gcc_0, SIG
+    out CCP, gcc_0
+    ldi gcc_0, 1
+    out CLKPSR, gcc_0
+    set_bit PCMSK, n64
+    ldi gcc_0, 1 << PCIE0
+    out PCICR, gcc_0
     ldi gcc_0, 5
     out TCCR0B, gcc_0
-    ldi gcc_0, 130
-    out OCR0A, gcc_0
-    sbi DDRB, n64l
-    sbi DDRB, gccl
+    ldi gcc_0, 64
+    out OCR0AL, gcc_0
+    ldi gcc_0, 1 << SE
+    out SMCR, gcc_0
+    set_bit DDRB, led
     clr n64_0
     clr n64_1
     clr n64_2
     clr n64_3
-    ldi gcc_1, 5
     sei
 ; Sleep and let interrupts do the work.
 main:
@@ -254,55 +307,45 @@ main:
 
 ; React to N64 request.
 pcint0:
-    cbi PORTB, n64l
-    cbi PORTB, gccl
-    clr gcc_0
-    in gcc_1, OSCCAL
-    n64_read
-    n64_read
-    n64_read
-    n64_read
-    n64_read
-    n64_read
-    n64_read
-    sbrs gcc_0, 0
-    out OSCCAL, gcc_1
+    delay_1_us
+    nop
+    read_bits n64, gcc_0, gcc_1, 7
+    nop
+    delay_1_us
     tst gcc_0
     breq status
     cpi gcc_0, 0x7f
     breq status
     cpi gcc_0, 1
     breq poll
+; Clear interrupt flag and exit.
+pcint0_done:
+    delay_1_us
+    clr_bit DDRB, n64
+    delay_1_us
+    ldi gcc_0, 1 << PCIF0
+    out PCIFR, gcc_0
     reti
 ; Send status response.
 status:
-    sbi PORTB, n64l
-    ldi gcc_1, 5
-    ldi gcc_2, 0
-    ldi gcc_3, 2
+    set_bit PORTB, led
+    ldi n64_1, 5
+    ldi n64_2, 0
+    ldi n64_3, 2
     nop
-    nop
-    nop
-    nop
-    nop
-    send_byte n64, gcc_1
-    send_byte n64, gcc_2
-    send_byte n64, gcc_3
-    n64_send_stop
+    rjmp pcint0_send_1
 ; Send poll response.
 poll:
-    sbi PORTB, gccl
     clr gcc_0
-    out TCNT0, gcc_0
-    sbr gcc_0, 4
-    out TIMSK, gcc_0
+    out TCNT0L, gcc_0
+    sbr gcc_0, OCIE0A
+    out TIMSK0, gcc_0
     send_byte n64, n64_0
+; Send remaining response bytes.
+pcint0_send_1:
     send_byte n64, n64_1
     send_byte n64, n64_2
     send_byte n64, n64_3
-    n64_send_stop
-; Releases the N64 data line and returns.
-n64_stop_wait:
-    delay_1_us
-    cbi DDRB, n64
-    reti
+    nop
+    set_bit DDRB, n64
+    rjmp pcint0_done
